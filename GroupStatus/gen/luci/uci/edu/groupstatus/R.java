ba8d2a9652378d94a9dd235769c2a53b7a9035d0
Jeff Lee <shihcl@uci.edu>
2014-04-30 01:07:18 -0700

    Create the welcome page and it's background

--- GroupStatus/gen/luci/uci/edu/groupstatus/R.java
+++ GroupStatus/gen/luci/uci/edu/groupstatus/R.java
@@ -22,12 +22,14 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int welcome_background=0x7f020001;
     }
     public static final class id {
         public static final int LinearLayout=0x7f080000;
         public static final int LinearLayout1=0x7f080006;
-        public static final int action_FrndMngr=0x7f08000a;
-        public static final int action_settings=0x7f080009;
+        public static final int WelcomePage=0x7f080009;
+        public static final int action_FrndMngr=0x7f08000b;
+        public static final int action_settings=0x7f08000a;
         public static final int button1=0x7f080002;
         public static final int edit=0x7f080001;
         public static final int listView=0x7f080007;
@@ -41,6 +43,7 @@ public final class R {
         public static final int friend_template=0x7f030001;
         public static final int listrow_details=0x7f030002;
         public static final int listrow_group=0x7f030003;
+        public static final int welcome_page=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -52,6 +55,7 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int defaul_Name=0x7f050004;
         public static final int defaul_Status=0x7f050005;
+        public static final int desc=0x7f050007;
         public static final int hello_world=0x7f050002;
         public static final int send=0x7f050003;
     }
