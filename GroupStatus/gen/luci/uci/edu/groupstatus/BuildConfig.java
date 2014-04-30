dc829645257c5462c7b7d15f9a3e311fc609cd39
Jeff Lee <shihcl@uci.edu>
2014-04-29 22:36:47 -0700

    Import from the previous project.

---
+++ GroupStatus/gen/luci/uci/edu/groupstatus/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package luci.uci.edu.groupstatus;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ GroupStatus/src/luci/uci/edu/groupstatus/Group.java
@@ -0,0 +1,15 @@
+package luci.uci.edu.groupstatus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Group {
+
+  public String string;
+  public final List<String> children = new ArrayList<String>();
+
+  public Group(String string) {
+    this.string = string;
+  }
+
+} 
