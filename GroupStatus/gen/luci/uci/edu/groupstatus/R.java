b59a7a71148ecab0dd952c44cb83315ffc388448
Jeff Lee <shihcl@uci.edu>
2014-04-30 23:51:18 -0700

    Add login and authentication UI/code

--- GroupStatus/gen/luci/uci/edu/groupstatus/R.java
+++ GroupStatus/gen/luci/uci/edu/groupstatus/R.java
@@ -22,21 +22,29 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int welcome_background=0x7f020001;
+        public static final int welcome_background_down=0x7f020001;
+        public static final int welcome_background_middle=0x7f020002;
+        public static final int welcome_background_up=0x7f020003;
     }
     public static final class id {
         public static final int LinearLayout=0x7f080000;
         public static final int LinearLayout1=0x7f080006;
-        public static final int WelcomePage=0x7f080009;
-        public static final int action_FrndMngr=0x7f08000b;
-        public static final int action_settings=0x7f08000a;
+        public static final int action_FrndMngr=0x7f080011;
+        public static final int action_settings=0x7f080010;
         public static final int button1=0x7f080002;
         public static final int edit=0x7f080001;
+        public static final int linearLayoutForLogIn=0x7f08000a;
         public static final int listView=0x7f080007;
         public static final int listView1=0x7f080003;
         public static final int listView2=0x7f080004;
         public static final int listView3=0x7f080005;
+        public static final int loginArea=0x7f08000b;
+        public static final int loginTextView=0x7f08000f;
+        public static final int space=0x7f08000c;
         public static final int textView1=0x7f080008;
+        public static final int userID=0x7f08000d;
+        public static final int userPW=0x7f08000e;
+        public static final int welcomePage=0x7f080009;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
