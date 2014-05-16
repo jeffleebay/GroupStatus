19b4e0ca61a1951940a28b2d2313352c48539394
Jeff Lee <shihcl@icd.uci.edu>
2014-05-16 00:05:22 -0700

    On the proccess of creating new status update UI

---
+++ GroupStatus/src/luci/uci/edu/groupstatus/StatusCollector.java
@@ -0,0 +1,12 @@
+package luci.uci.edu.groupstatus;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StatusCollector extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.status_collector);
+	}
+}
