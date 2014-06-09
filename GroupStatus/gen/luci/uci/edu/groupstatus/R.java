edf13e21ac97e657906cc68a20430b09526868db
Jeff Lee <shihcl@icd.uci.edu>
2014-06-08 17:29:01 -0700

    Update all icons with Android Design Icons

--- GroupStatus/gen/luci/uci/edu/groupstatus/R.java
+++ GroupStatus/gen/luci/uci/edu/groupstatus/R.java
@@ -33,16 +33,17 @@ public final class R {
     public static final class drawable {
         public static final int application_background=0x7f020000;
         public static final int checked=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int ic_launcher_notification=0x7f020003;
-        public static final int ic_launcher_original=0x7f020004;
-        public static final int location=0x7f020005;
-        public static final int noise=0x7f020006;
-        public static final int toast_border=0x7f020007;
-        public static final int welcome_background_down=0x7f020008;
-        public static final int welcome_background_middle=0x7f020009;
-        public static final int welcome_background_up=0x7f02000a;
-        public static final int wifi=0x7f02000b;
+        public static final int ic_action_edit=0x7f020002;
+        public static final int ic_action_location_found=0x7f020003;
+        public static final int ic_action_mic=0x7f020004;
+        public static final int ic_action_network_wifi=0x7f020005;
+        public static final int ic_action_paste=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int ic_launcher_notification=0x7f020008;
+        public static final int toast_border=0x7f020009;
+        public static final int welcome_background_down=0x7f02000a;
+        public static final int welcome_background_middle=0x7f02000b;
+        public static final int welcome_background_up=0x7f02000c;
     }
     public static final class id {
         public static final int Button_GroupStatus_Update=0x7f090058;
